8fa1ecc7584a454e6138144374fee8b7984da3ef
你好2007 <2501482523@qq.com>
2022-05-29 18:09:54 +0800

    调整主体结构

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,15 +1,7 @@
-import { Component, ref } from 'nefbl'
-import { isFunction } from "@hai2007/tool/type"
-
-import urlFormat from '../tool/urlFormat'
+import { Component } from 'nefbl'
 
 import style from './index.scss'
 import template from './index.html'
-
-let pages = {
-    "menu": () => import('./menu/index'),
-    "size": () => import('./size/index')
-}
 
 @Component({
     selector: "app-root",
@@ -22,29 +14,14 @@ export default class {
 
     $setup() {
         return {
-            currentPage: ref(null)
+
         }
     }
 
     $mounted() {
 
         // 地址栏信息
-        let urlObj = urlFormat(globalThis.location.href)
 
-        // 获取当前页面
-        let page = pages[urlObj.router[0]]
-
-        // 或者页面不存在
-        if (!isFunction(page)) {
-
-            // 如果地址错误，跳转到菜单页面
-            page = pages['menu']
-        }
-
-        // 打开页面
-        page().then(data => {
-            this.currentPage = data.default
-        })
 
     }
 
--- src/tool/openPage.js
+++
@@ -1,6 +0,0 @@
-export default url => {
-    let aDom = document.createElement('a')
-    aDom.setAttribute('href', url)
-    aDom.setAttribute('target', '_blank')
-    aDom.click()
-}
